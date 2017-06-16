74550f7c16f228f44e1324cd0090e73aacb2741c
tknk0786 <21411187st@tama.ac.jp>
2017-06-16 11:44:13 +0900

    teisyutusimasu

--- hello.php
+++ hello.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+ <head>
+ <meta charset="utf-8">
+ <title></title>
+ </head>
+ <body>
 
 <?php
-
-var_dump($_GET);
+if( !isset($_GET['id'],$_GET['name']) ||
+ strlen($_GET['id']) == 0 ||
+ strlen($_GET['name']) == 0
+)
+{
+ echo 'Missing Data';
+}
+else //¬‚è—§‚½‚È‚©‚Á‚½ê‡
+{
+print $_GET['id'];
+print ('no');
+print $_GET['name'];
+print ('sann,konnnichiha');
+ }
 
 ?>
 
 
-  </body>
+ </body>
 </html>
